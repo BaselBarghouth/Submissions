949be68ccbfd39f6d5e6172217e99f9023b8517c
Your Name <your.email@example.com>
2019-05-21 15:24:42 +0300

    External files and Simple Math

---
+++ Exercises/Javascript-Basics-01/Step_6.js
@@ -0,0 +1,4 @@
+var first_number=parseFloat(prompt("Enter the first number"));
+var second_number=parseFloat(prompt("Enter the second number"));
+var validate=first_number%second_number;
+alert(validate);
