6e74a01da50ac4b61714ef36c6ebb038fcfbcf38
Your Name <your.email@example.com>
2019-06-11 14:01:10 +0300

    step3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -3,9 +3,24 @@ import dcb from './DataBase/DataBase';
 // Set up the express app
 const app = express();
 // get all todos
+
 app.get("/",(req, res) => {
   res.send("OK")
 });
+app.get("/test",(req, res) => {
+
+    res.send({
+        status:200, 
+        message:"ok"
+    })
+  });
+  app.get("/time",(req, res) => {
+    var t = new Date()
+    res.send({
+        status:200, 
+        message:t.getHours()+':'+t.getMinutes()+':'+t.getSeconds()
+    })
+  });
 const PORT = 5000;
 
 app.listen(PORT, () => {
