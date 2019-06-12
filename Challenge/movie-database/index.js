81187d2d2f0af96ad0ead75889bd987d1a9cec70
Your Name <your.email@example.com>
2019-06-12 09:12:24 +0300

    step4

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -21,6 +21,25 @@ app.get("/test",(req, res) => {
         message:t.getHours()+':'+t.getMinutes()+':'+t.getSeconds()
     })
   });
+  app.get("/hello/:id",(req, res) => {
+    const id = req.params.id;
+    var t = new Date()
+    res.send({
+        status:200, 
+        message:'Hello, ' +id
+    })
+  });
+  app.get("/search",(req, res) => {
+    const s = req.query.s;
+    let data;
+    let status=200;
+    !s ?  (data='you have to provide a search',status=500)  : (data=s)
+    res.send({
+      status:status, 
+      message:'hello',
+      data:data
+  })
+});
 const PORT = 5000;
 
 app.listen(PORT, () => {
